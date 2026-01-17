fd6a9afbbad09fd61ffc99d10dd6bee3387f257c
ricardoibarronjr-dev <ricardoibarronjr@gmail.com>
2026-01-17 01:35:52 +0000

    Done!

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,10 @@
-pass
+def calculate_average(num1, num2, num3):
+    return((num1+num2+num3)/3)
+
+
+def add_tax(amount):
+    return(amount*1.10)
+
+def greet_user(name):
+    name= "Hello "+ name
+    return name
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,19 @@
-pass
+def check_multiple(number):
+    return number%5==0 and number%3==0
+
+
+def check_password(input_string):
+  if input_string == "Python123":
+    return "access granted"
+  else:
+    return "access denied"
+  
+def calculate_federal_tax(salary):
+  if salary<= 11000 :
+    return salary*0.10
+  elif salary>11000 and salary<=44725:
+    return salary*0.12
+  elif salary>44725 and salary<=95375:
+    return salary*0.22
+  else: salary>95375
+  return salary*0.24
